d6eb0ff5912f99c79a271e28dfe763af2226a0f5
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-03-06 13:35:22 -0800

    habitat route and model

--- models/habitat.js
+++ models/habitat.js
@@ -1,3 +1,42 @@
 const db = require('../db');
 const env = require('../config');
 const axios = require('axios');
+const { getDescription } = require('../helpers/helpers');
+
+// habitat table contains (name, description)
+
+class Habitat {
+  static async getAllHabitats() {
+    let result = await db.query(`SELECT * FROM habitats`);
+    return result.rows;
+  }
+  static async getSpecificHabitat(name) {
+    // Needs to get all pokemon that live in the habitat too
+    // Use a join on the search with the join table, full join
+    let response = await db.query(`SELECT * FROM habitats WHERE name = $1`, [
+      name
+    ]);
+    let result = response.rows[0];
+    if (!result) result = await Habitat.getSpecificFromAPI(name);
+    return result;
+  }
+  static async getSpecificFromAPI(name) {
+    let response = await axios.get(`${env.POKEURL}/pal-park-area/${name}`);
+    let description = getDescription(response.data.name);
+    // let pokemon = response.data.pokemon_encounters.map((p)=> p.pokemon_species)
+    let habitat = {
+      name: response.data.name,
+      description
+    };
+    return await Habitat.insertHabitatToDB(habitat);
+  }
+  static async insertHabitatToDB(habitat) {
+    // Inserts into habitats table
+    return await db.query(
+      `INSERT INTO habitats (name, description) VALUES ($1, $2) RETURNING *`,
+      [habitat.name, habitat.description]
+    );
+  }
+}
+
+module.exports = Habitat;
--- routes/habitats.js
+++ routes/habitats.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const router = new express.Router();
 const Habitat = require('../models/habitat');
+
+// get all habitat data (excluding pokemon that live there)
+
+router.get('/', async function(req, res, next) {
+  let habitats = await Habitat.getAllHabitats();
+  return res.json({ habitats });
+});
+// get specific habitat data (including pokemon that live there)
+router.get('/:habitat', async function(req, res, next) {
+  let habitats = await Habitat.getSpecificHabitat(req.params.habitat);
+  return res.json({ habitats });
+});
+
+module.exports = router;
