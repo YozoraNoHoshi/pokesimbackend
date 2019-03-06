227fd73c0f67fc76b1877935605a91a776e45f3a
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-03-06 13:35:28 -0800

    pokemon route and model

---
+++ routes/pokemon.js
@@ -0,0 +1,4 @@
+// BATTLE - Get random pokemon data from habitat
+
+// POKEDEX - Get specific pokemon data from db, maybe pokemon habitats
+// POKEDEX - Get all pokemon data in ID order, including habitats they live in
