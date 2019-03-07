a1c5e4fc0f964af49e12803598e9d39be389aa76
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2019-03-06 21:34:16 -0800

    query builder

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -8,4 +8,30 @@ function getDescription(habitatName) {
   return 'No data found for this location.';
 }
 
-module.exports = { getDescription };
+/**
+ *  Builds a query for inserting via SQL
+ * @param {string} table
+ * @param {array} columns items should be strings.
+ * @param {array} items array of objects. Each object should contain the strings from columns.
+ */
+function buildMultipleInsert(table, columns, items) {
+  let columnHeaders = columns.join(', ');
+  let counter = 1;
+  let entries = [];
+  let values = [];
+  for (let item of items) {
+    let entry = [];
+    for (let column of columns) {
+      entry.push(`$${counter}`);
+      values.push(item[column]);
+      counter += 1;
+    }
+    entries.push(`(${entry.join(', ')})`);
+  }
+  let finalValues = entries.join(', ');
+
+  let query = `INSERT INTO ${table} (${columnHeaders}) VALUES ${finalValues} RETURNING *`;
+  return { query, values };
+}
+
+module.exports = { getDescription, buildMultipleInsert };
